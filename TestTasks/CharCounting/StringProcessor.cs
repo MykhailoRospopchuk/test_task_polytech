a00ddb632c5a1aa6d8650925bef5e7d0668488c1
mykhailo_u <rosmisha@gmail.com>
2025-02-06 23:56:52 +0200

    during first one

--- TestTasks/CharCounting/StringProcessor.cs
+++ TestTasks/CharCounting/StringProcessor.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace TestTasks.VowelCounting
+namespace TestTasks.CharCounting
 {
+    using System.Linq;
+
     public class StringProcessor
     {
+        private static readonly int ChunkSize = 5 * 1024 * 1024;
+
         public (char symbol, int count)[] GetCharCount(string veryLongString, char[] countedChars)
         {
-            throw new NotImplementedException();
+            if (countedChars == null || countedChars.Length == 0 || string.IsNullOrEmpty(veryLongString))
+            {
+                return Array.Empty<(char symbol, int count)>();
+            }
+
+            var length = veryLongString.Length;
+            var distinctChars = new HashSet<char>(countedChars);
+            var rawResult = new Dictionary<char, int>(distinctChars.ToDictionary(i => i, _=> 0));
+
+            var stringReadOnlyMemory = veryLongString.AsMemory();
+
+            foreach (var chunk in GetSplit(stringReadOnlyMemory, length))
+            {
+                foreach (var cahr in chunk.Span)
+                {
+                    if (rawResult.TryGetValue(cahr, out int count))
+                    {
+                        rawResult[cahr] = count + 1;
+                    }
+                }
+            }
+
+            return rawResult.Select(kv => (kv.Key, kv.Value)).ToArray();
+        }
+
+        private IEnumerable<ReadOnlyMemory<char>> GetSplit(ReadOnlyMemory<char> stringReadOnlyMemory, int length)
+        {
+            for (int i = 0; i <= length; i += ChunkSize)
+            {
+                yield return stringReadOnlyMemory.Slice(i, Math.Min(ChunkSize, length - i));
+            }
         }
     }
 }
--- TestTasks/Program.cs
+++ TestTasks/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using TestTasks.InternationalTradeTask;
-using TestTasks.VowelCounting;
+using TestTasks.CharCounting;
 using TestTasks.WeatherFromAPI;
 
 namespace TestTasks
@@ -13,15 +13,17 @@ namespace TestTasks
         {
             //Below are examples of usage. However, it is not guaranteed that your implementation will be tested on those examples.            
             var stringProcessor = new StringProcessor();
-            string str = File.ReadAllText("./StringExample.txt");
-            var charCount = stringProcessor.GetCharCount(str, new char[] { 'l', 'r', 'm' });
+            string str = File.ReadAllText("./CharCounting/StringExample.txt");
+            var charCount = stringProcessor.GetCharCount(str, new char[] { 'l', 'r', 'm', 'L', 'R', 'M'});
 
-            var commodityRepository = new CommodityRepository();
-            commodityRepository.GetImportTariff("Natural honey");
-            commodityRepository.GetExportTariff("Iron/steel scrap not sorted or graded");            
+            // var commodityRepository = new CommodityRepository();
+            // commodityRepository.GetImportTariff("Natural honey");
+            // commodityRepository.GetExportTariff("Iron/steel scrap not sorted or graded");            
+            //
+            // var weatherManager = new WeatherManager();
+            // var comparisonResult = await weatherManager.CompareWeather("kyiv,ua", "lviv,ua", 4);
 
-            var weatherManager = new WeatherManager();
-            var comparisonResult = await weatherManager.CompareWeather("kyiv,ua", "lviv,ua", 4);
+            Console.ReadKey();
         }
     }
 }
